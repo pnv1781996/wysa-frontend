51ddee1f0b3628d0d3d0155e2040e4dc4d925d69
pnv1781996 <pp4560900@gmail.com>
2023-06-20 10:41:25 +0530

    add url

--- src/API/http-common.js
+++ src/API/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosApi = axios.create({
-  baseURL: "http://localhost:3001/api/",
+  baseURL: "https://wysa-backend.vercel.app/api/",
   headers: {
     "Content-type": "application/json",
   },
